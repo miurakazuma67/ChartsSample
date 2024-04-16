910ac7a206bf754faa8ae1a850413545f37cb4f6
三浦一真 <miurakazuma67@gmail.com>
2024-04-16 21:58:35 +0900

    Initial Commit

---
+++ ChartsSample/ChartsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsSampleApp.swift
+//  ChartsSample
+//
+//  Created by 三浦一真 on 2024/04/16.
+//
+
+import SwiftUI
+
+@main
+struct ChartsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4688664cea1c59db54a8240460ea8593482f0f0d
三浦一真 <miurakazuma67@gmail.com>
2024-05-06 22:35:26 +0900

    component

--- ChartsSample/ShowWeekDataView.swift
+++ ChartsSample/ShowWeekDataView.swift
@@ -10,31 +10,35 @@ import SwiftUI
 // カテゴリ押下したらセルごと色変わってテキストも白くする
 struct ShowWeekDataView: View {
     var body: some View {
-        LazyHGrid(rows: /*@START_MENU_TOKEN@*/[GridItem(.fixed(20))]/*@END_MENU_TOKEN@*/, content: {
-            VStack(content: {
-                Text("color")
-                Rectangle()
-                    .frame(height: 1)  // 線の厚さを1に設定
-                    .foregroundColor(.gray)  // 線の色をグレーに設定
-                Text("category")
-            })
-                .frame(width: 100, height: 100)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)  // 角の丸い枠線
-                        .stroke(Color.blue, lineWidth: 2)
-                )
+        LazyVGrid(columns: [GridItem(.fixed(300))], alignment: .leading, content: {
+//            VStack(content: {
+//                Text("color")
+//                Rectangle()
+//                    .frame(height: 1)  // 線の厚さを1に設定
+//                    .foregroundColor(.gray)  // 線の色をグレーに設定
+//                Text("category")
+//            })
+//                .frame(width: 100, height: 100)
+//                .overlay(
+//                    RoundedRectangle(cornerRadius: 10)  // 角の丸い枠線
+//                        .stroke(Color.blue, lineWidth: 2)
+//                )
+            
+            TagView(color: Color.green, title: "Swift")
+            // データ受け取れるようにする
+            
 // ------------------- 曜日＋学習時間 S-------------------------------------
             // forループでデータの個数(1週間分)生成
             HStack(spacing: 0) {
                 ForEach(0..<7, id: \.self) { _ in
                     VStack {
                         Text("日")
-                            .font(.system(size: 14))
+                            .font(.system(size: 12))
                         Rectangle()
                             .frame(height: 1)
                             .foregroundColor(.gray)
                         Text("10分")
-                            .font(.system(size: 14))
+                            .font(.system(size: 12))
                     }
                     .padding(8)
                     .background(Color(UIColor.tertiarySystemGroupedBackground))
--- ChartsSample/TagView.swift
+++ ChartsSample/TagView.swift
@@ -5,36 +5,19 @@
 //  Created by 三浦一真 on 2024/05/04.
 //
 
-import UIKit
+import SwiftUI
 
-class TagView: UIView {
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.textAlignment = .center
-        label.textColor = .white
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    init(frame: CGRect, color: UIColor, title: String) {
-        super.init(frame: frame)
-        self.backgroundColor = color
-        self.layer.cornerRadius = frame.height / 2
-        self.translatesAutoresizingMaskIntoConstraints = false
-
-        setupTitleLabel(with: title)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    private func setupTitleLabel(with title: String) {
-        addSubview(titleLabel)
-        NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor)
-        ])
-        titleLabel.text = title
+struct TagView: View {
+    var color: Color
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.caption)
+            .foregroundColor(.white)
+            .padding(.horizontal, 10)
+            .padding(.vertical, 5)
+            .background(color)
+            .cornerRadius(10)
     }
 }
